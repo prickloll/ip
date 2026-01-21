66c491fc868449447c0cf0c0ba614282f60563b7
Eric <ericlawlf@gmail.com>
2026-01-21 21:43:33 +0800

    Level-1: Initialised Scanner and loop for user input

--- src/main/java/Eric.java
+++ src/main/java/Eric.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 public class Eric {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Eric\nWhat can I do for you?");
+        //For receiving inputs from the user
+        Scanner scanner = new Scanner(System.in);
+
+        //Main Execution: Exit code when user input equals "bye"
+        while (true) {
+            String user_input = scanner.nextLine();
+            if (user_input.equals("bye")) {
+                break;
+            }
+        }
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
