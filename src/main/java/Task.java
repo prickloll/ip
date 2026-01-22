21cc60c5fd707c74d89faa90d76733576afd480c
Eric <ericlawlf@gmail.com>
2026-01-22 11:22:22 +0800

    Level-3: Add Task class in Task.java to represent tasks as an object

---
+++ src/main/java/Task.java
@@ -0,0 +1,31 @@
+/**
+ * Represents a task that can be marked as completed or not.
+ */
+
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public void markDone() {
+        this.isDone = true;
+    }
+
+    public void markUndone() {
+        this.isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + getStatusIcon() + "] " + description;
+    }
+
+}
