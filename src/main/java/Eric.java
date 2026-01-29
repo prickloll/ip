2f5471836bb4b4daf4bae1d4512cc9ebe31669cb
Eric <ericlawlf@gmail.com>
2026-01-29 18:58:56 +0800

    A-MoreOOP: Add and apply Parser class to program

--- src/main/java/Eric.java
+++ src/main/java/Eric.java
@@ -8,7 +8,7 @@ public class Eric {
     private static Repository repo  = new Repository("./data/Eric.txt");
     private static TaskList tasks;
     private static Ui ui;
-
+    
     /**
      * Starts the chatbot and handles the main execution loop.
      *
@@ -30,51 +30,18 @@ public class Eric {
         ui.greeting();
 
         Scanner scanner = new Scanner(System.in);
-
-        while (true) {
+        boolean exit = false;
+        while (!exit) {
             String userInput = scanner.nextLine();
-            if (userInput.equals("bye")) {
-                ui.bye();
-                break;
-            }
             try {
-                command(userInput);
-                repo.save(tasks.getEveryTask());
+                exit = Parser.parse(userInput, tasks, ui, repo);
+
             } catch (EricException e) {
                 ui.linebreak();
                 ui.errorMsg(e.getMessage());
                 ui.linebreak();
             }
 
-        }
-    }
-
-    /**
-     * Matches the user input to the appropriate task action.
-     *
-     * @param userInput The text entered by the user.
-     * @throws EricException If the command or its relevant parameters are invalid.
-     */
-    public static void command(String userInput) throws EricException{
-        if (userInput.startsWith("todo")){
-            ui.displayTaskAdded(tasks.addTodo(userInput), tasks.getSize());
-        } else if (userInput.startsWith("deadline")){
-            ui.displayTaskAdded(tasks.addDeadline(userInput), tasks.getSize());
-        } else if (userInput.startsWith("event")){
-            ui.displayTaskAdded(tasks.addEvent(userInput), tasks.getSize());
-        } else if (userInput.startsWith("mark") || userInput.startsWith("unmark")) {
-            boolean isMark = userInput.startsWith("mark");
-            ui.displayMarked(tasks.setMarkUnmarked(userInput), isMark);
-        } else if (userInput.equals("list")) {
-            ui.displayTaskList(tasks.getEveryTask());
-        } else if (userInput.startsWith("delete")) {
-            ui.displayTaskAdded(tasks.deleteTask(userInput), tasks.getSize());
-        } else if (userInput.startsWith("find")) {
-            ArrayList<Task> results = tasks.findTasksByDate(userInput);
-            ui.displaySearch(results, userInput.split(" ")[1]);
-        }
-        else {
-            throw new EricException("Sorry, I can't seem to handle your request!");
         }
     }
 
